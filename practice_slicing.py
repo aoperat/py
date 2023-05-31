9407cb563c2c3cc0dc0362f01a8bae9397b594f1
aoperat <54986046+aoperat@users.noreply.github.com>
2023-05-31 09:19:44 +0900

    slicing

---
+++ practice_slicing.py
@@ -0,0 +1,9 @@
+jumin = "910711=1234567"
+
+print("성별 : " + jumin[7])
+print("연 : " + jumin[0:2])
+print("원 : " + jumin[2:4])
+print("일 : " + jumin[4:6])
+
+print("생년월일 : " + jumin[:6])
+print("뒤 7자리 : " + jumin[7:])
