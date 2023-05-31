eeb315925c65d09dd93f44d508e65352de2b17fe
aoperat <54986046+aoperat@users.noreply.github.com>
2023-05-31 09:11:04 +0900

    기본문법 연습

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,27 @@
+
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+
 print("hello world")
+
+print( "우리집 " + animal + "의 이름은 " + name + "에요")
+
+'''
+이게 주석이 돼?
+'''
+
+print(5//3) #목을 구하는 법
+print((abs(-5))) # 절대값 구하는 법 
+print(pow(4,2)) #제곱을 구하는법 4^2 = 4*4 = 16
+
+print(max(5,2))
+print(min(5,2))
+print(round(3.14))
+
+from math import *
+print(floor(4.9))
+print(ceil(4.9))
+print(sqrt(16))  
