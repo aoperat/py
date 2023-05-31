6cf5d66c245c0b909a6d4df718a6a905308dcb96
aoperat <54986046+aoperat@users.noreply.github.com>
2023-05-31 09:52:40 +0900

    list 연습

---
+++ practice/practice_list.py
@@ -0,0 +1,47 @@
+#리스트 []
+
+# 지하철 칸별로 10명, 20명, 30명
+
+# subway1 = 10
+# print(subway1)
+
+#subway = [10,20,30]
+#print(subway)
+
+
+# subway = ["jay","colin", "josh"]
+# print(subway)
+
+# print(subway.index("jay"))
+
+# subway.append("gavin")
+# print(subway)
+# print(subway.index("gavin"))
+
+# subway.insert(1,"laura")
+# print(subway)
+
+# print(subway.pop())
+# print(subway)
+
+# print(subway.pop())
+# print(subway)
+
+# print(subway.pop())
+# print(subway)
+
+# # 같은 이름의 사람이 몇 명 있는지 확인
+# subway.append("jay")
+# print(subway)
+# print(subway.count("jay"))
+
+#정렬
+# num_list = [5,3,1,4,2]
+# num_list.sort()
+# print(num_list)
+
+# num_list.reverse()
+# print(num_list)
+
+# num_list.clear()
+# print(num_list)
