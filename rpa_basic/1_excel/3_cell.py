4f27d3ae7e0dea59831e687744fd0d1f25243c02
aoperat <54986046+aoperat@users.noreply.github.com>
2023-05-31 13:46:49 +0900

    엑셀 기본2

--- rpa_basic/1_excel/3_cell.py
+++ rpa_basic/1_excel/3_cell.py
@@ -26,4 +26,15 @@ print(ws.cell(row=1, column=2).value)
 
 c = ws.cell(column=3, row=1, value=10)
 print(c.value)
+
+from random import *
+
+# 반복문을 이용해서 랜덤 숫자 채우기
+index = 1
+for x in range(1,11): # 10개 row
+    for y in range(1,11):
+        ws.cell(row=x, column = y, value = randint(0,100))
+        ws.cell(row=x, column = y, value = index)
+        index += 1
+
 wb.save("sample.xlsx")
